0ee4c51f06adc7391af39cf3262b03176a1b71d6
ErlanggaOfficial <bintanggerlanggaa@gmail.com>
2025-08-26 18:10:58 +0700

    Update auth-utils.d.ts

--- lib/Utils/auth-utils.d.ts
+++ lib/Utils/auth-utils.d.ts
@@ -1,24 +1,12 @@
 import type { Logger } from 'pino';
-import type {
-    AuthenticationCreds,
-    CacheStore,
-    SignalKeyStore,
-    SignalKeyStoreWithTransaction,
-    TransactionCapabilityOptions
-} from '../Types';
-
+import type { AuthenticationCreds, CacheStore, SignalKeyStore, SignalKeyStoreWithTransaction, TransactionCapabilityOptions } from '../Types';
 /**
  * Adds caching capability to a SignalKeyStore
  * @param store the store to add caching to
  * @param logger to log trace events
  * @param _cache cache store to use
  */
-export declare function makeCacheableSignalKeyStore(
-    store: SignalKeyStore,
-    logger: Logger,
-    _cache?: CacheStore
-): SignalKeyStore;
-
+export declare function makeCacheableSignalKeyStore(store: SignalKeyStore, logger: Logger, _cache?: CacheStore): SignalKeyStore;
 /**
  * Adds DB like transaction capability (https://en.wikipedia.org/wiki/Database_transaction) to the SignalKeyStore,
  * this allows batch read & write operations & improves the performance of the lib
@@ -26,21 +14,5 @@ export declare function makeCacheableSignalKeyStore(
  * @param logger logger to log events
  * @returns SignalKeyStore with transaction capability
  */
-export declare const addTransactionCapability: (
-    state: SignalKeyStore,
-    logger: Logger,
-    options: TransactionCapabilityOptions
-) => SignalKeyStoreWithTransaction;
-
-/**
- * Initialize new auth credentials
- */
+export declare const addTransactionCapability: (state: SignalKeyStore, logger: Logger, { maxCommitRetries, delayBetweenTriesMs }: TransactionCapabilityOptions) => SignalKeyStoreWithTransaction;
 export declare const initAuthCreds: () => AuthenticationCreds;
-
-/**
- * Initialize new auth credentials with multi-session support
- * @param sessionId unique ID for the session (default = "default")
- */
-export declare const initMultiAuthCreds: (
-    sessionId?: string
-) => AuthenticationCreds & { sessionId: string };
